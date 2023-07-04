6d0677512f67b9f2cbb9cb00f49a0338cbab3265
Tochukwu Ukpai <tochukwuukpai@gmail.com>
2023-07-04 10:04:19 +0100

    Initial Commit

---
+++ SnowResorts/SnowResortsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowResortsApp.swift
+//  SnowResorts
+//
+//  Created by Theós on 04/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SnowResortsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
